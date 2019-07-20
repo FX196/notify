f98c1220e5c9042a11307c5dbe366b54922ddd01
yuhongc <mikechen212@gmail.com>
2019-07-20 14:41:12 -0700

    update

--- send_notification.py
+++ send_notification.py
@@ -3,15 +3,18 @@ import requests
 
 def notify(message):
     with open('token', 'r') as file:
-        token = file.read()
+        token = file.readline()[:-1]
     report = {}
     report['value1'] = message
     requests.post('https://maker.ifttt.com/trigger/job_done/with/key/%s' % token, data=report)
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        notify(sys.argv[1])
+        message = sys.argv[1]
+        notify(message)
     else:
         message = "Job %s done on %s" % (sys.argv[1], sys.argv[2])
         notify(message)
-        # e.g. "Job upload done on local" 
+        # e.g. "Job upload done on local"
+    print("Sent message: %s" % message)
+
